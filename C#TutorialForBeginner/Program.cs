6ba7d8193dbf7418d36674ae01a783475ae1770d
Majid Khadim <Majid Khadim@MAJIDKHADIM>
2024-03-25 12:37:25 +0500

    Add project files.

---
+++ C#TutorialForBeginner/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace C_TutorialForBeginner
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("I am learning C# for beginners");
+        }
+    }
+}
