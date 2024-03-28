e71602ccb9fc3df59dee5079bae8f5c0e56bb065
Majid Khadim <110695542+MajidKhadim@users.noreply.github.com>
2024-03-28 13:09:08 +0500

    Add files via upload

---
+++ Part21/Class21.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_TutorialForBeginner.Part21
+{
+    public class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+
+        public Employee()
+        {
+            Console.WriteLine("Employee class");
+        }
+        public Employee(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        public void PrintFullName()
+        {
+            Console.WriteLine(FirstName + LastName);
+        }
+    }
+
+    public class FullTimeEmployee:Employee 
+    {
+        float YearlySalary;
+        public FullTimeEmployee() : base("derived class controlling parent class")
+        {
+            Console.WriteLine("child class");
+        }
+
+    }
+    public class PartTimeEmployee : Employee
+    {
+        float HourlySalary;
+
+    }
+}
---
+++ Part22/Class22.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_TutorialForBeginner.Part22
+{
+    public class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public void PrintFullName()
+        {
+            Console.WriteLine(FirstName + LastName);
+        }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        float YearlySalary;
+        public FullTimeEmployee()
+        {
+            Console.WriteLine("child class");
+        }
+        public new void PrintFullName()
+        {
+            Console.WriteLine(FirstName + LastName+"child class");
+        }
+    }
+    public class PartTimeEmployee : Employee
+    {
+        float HourlySalary;
+        public new void PrintFullName()
+        {
+            Console.WriteLine(FirstName + LastName + "child class");
+        }
+
+    }
+}
