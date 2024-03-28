1b169475efdbf841618706c446d29fed3fe43326
Majid Khadim <110695542+MajidKhadim@users.noreply.github.com>
2024-03-28 12:39:18 +0500

    Add files via upload

---
+++ Part20/Class20.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_TutorialForBeginner.Part20
+{
+    internal class Class20
+    {
+        string Name;
+        int? RollNo;
+        public Class20()
+        {
+            this.RollNo = null;
+            this.Name = null;
+            Console.WriteLine("Main bhi");
+        }
+
+        static Class20()
+        {
+            Console.WriteLine("static hoo main");
+        }
+
+        
+        public void PrintMyName()
+        {
+            Console.WriteLine($"My Name is {Name} and {RollNo}");
+        }
+
+    }
+}
