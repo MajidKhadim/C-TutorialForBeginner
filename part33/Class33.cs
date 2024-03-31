c20dc665c0961f6cc51b2a9e14ca5e771cc419a3
Majid Khadim <110695542+MajidKhadim@users.noreply.github.com>
2024-03-31 23:44:08 +0500

    files added

---
+++ part33/Class33.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_TutorialForBeginner.part33
+{
+    public interface IData
+    {
+        void PrintInterface();
+    }
+
+    public abstract class Data
+    {
+        public int DataProp {  get; set; }
+        public abstract void PrintAbstract();
+    }
+}
